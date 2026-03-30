a6bfb1850b6d3d7bf3cfe0362224a04b04ccf1c7
rsdrahi <rsdrahi13@gmail.com>
2026-03-30 15:29:16 +0600

    navbar responsive for mobile device

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-      <nav className='flex justify-between items-center max-w-7xl m-auto'>
+      <nav className='md:flex justify-between items-center max-w-7xl m-auto'>
         <h3 className='text-3xl font-bold bg-gradient-to-r from-[#4F39F6] to-[#9514FA]  bg-clip-text text-transparent'>DigiTools</h3>
         <div>
-          <ul className='flex gap-8 font-semibold text-[#101727]'>
+          <ul className='md:flex gap-8 font-semibold text-[#101727]'>
             <li>Products</li>
             <li>Features</li>
             <li>Pricing</li>
@@ -14,7 +14,7 @@ const Navbar = () => {
             <li>FAQ</li>
           </ul>
           </div>
-          <div className='flex gap-5 items-center'>
+          <div className='md:flex gap-5 items-center'>
             <ShoppingCart></ShoppingCart>
             <h3 className='font-semibold text-[#101727]'>Login</h3>
             <button className='btn rounded-full bg-gradient-to-r from-[#4F39F6] to-[#9514FA] text-white'>Get Started</button>
